26d6519e87bd08d594373c9f6e24cb7f2507a3dc
lahcen404 <lahcen.maskour2003@gmail.com>
2025-07-02 00:29:23 +0100

    Create CRUD Learner

--- src/main/java/com/ENAA_Skills/enaa_skills/controller/LearnerProgressController.java
+++ src/main/java/com/ENAA_Skills/enaa_skills/controller/LearnerProgressController.java
@@ -3,7 +3,7 @@ package com.ENAA_Skills.enaa_skills.controller;
 import org.springframework.stereotype.Repository;
 
 
-@Repository
-public interface LearnerProgressController {
+
+public class LearnerProgressController {
 
 }
